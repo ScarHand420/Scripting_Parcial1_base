3b44a080b2f1a5b99c2776cbee7edb3b82bed19d
DARK_ETERNAL <tomailsandstuff@gmail.com>
2019-02-10 11:21:23 -0500

    Add project files.

---
+++ Scripting_201910_Parcial1_base/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Scripting_201910_Parcial1_base
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
